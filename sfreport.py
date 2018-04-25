b98919f7f4b259f1336d5ab07efa59f0c0f6cd5e
rmiller1976 <rmiller@starfishstorage.com>
2018-04-25 17:54:20 -0400

    modified:   sfreport.py

--- sfreport.py
+++ sfreport.py
@@ -30,12 +30,9 @@
 #
 #********************************************************
 #  Author Ryan Miller
-#  Last modified 2018-04-10
+#  Last modified 2018-04-25
 #
 #  Report generation framework for Starfish
-
-
-
 
 #********************************************************
 #Import required modules
@@ -53,30 +50,36 @@ import argparse # Parser for command line options
 #********************************************************
 # Define fixed variables (Use sparingly!)
 
-logroot="/opt/starfish/log/sfreports/" # NEEDS TRAILING '/'
-reports_dir="reports/"                 # NEEDS TRAILING '/'
+sfconfigfile="/opt/starfish/etc/99-local.ini"  
+logroot="/opt/starfish/log/sfreports/"        # NEEDS TRAILING '/'
+reports_dir="reports/"                        # NEEDS TRAILING '/'
 
 #********************************************************
 # Define functions
 
-def getpgauth():
+def readconfigfile(__configfile,*argv):
     try:
         config = ConfigParser.ConfigParser()
-        config.read("/opt/starfish/etc/99-local.ini")
-        return(config.get('pg','pg_uri'))
-    except:
-        print('FATAL: Can\'t read config file to get connection uri')
+        config.read(__configfile)
+        return(config.get(*argv))
+    except Exception, e:
+        logentry(logfile,'FATAL: Can\'t read config file '+str(__configfile))
+        logentry(logfile,e)
         sys.exit(1)
 
-def logentry(__fn__,__st__):
-    with open(__fn__, "a") as f:
-        __ts__=time.time()
-        __ds__=datetime.datetime.fromtimestamp(ts).strftime("%Y%m%d-%H%M%S")
-        f.write(__ds__+":  "+str(__st__)+'\n')
+def logentry(__filein,__txtin):
+    with open(__filein, "a") as f:
+        __ts=time.time()
+        __ds=datetime.datetime.fromtimestamp(__ts).strftime("%Y%m%d-%H%M%S")
+        f.write(__ds+":  "+str(__txtin)+'\n')
+        print (__txtin)
 
 #************************************************************
-# Start here
+# Start here                                                #
+#************************************************************
 
+# Parse command line arguments
+# ----------------------------
 parser = argparse.ArgumentParser(description='Run a report query for Starfish')
 parser.add_argument('email', type=str, metavar='{recipient(s)}', nargs=1, help='Email report to {recipient(s)} (comma separated)')
 parser.add_argument('query', metavar='{filename}', nargs=1, help='File containing SQL query to run')
@@ -85,66 +88,111 @@ parser.add_argument('--delimiter', type=str, default=',', metavar='"x"', help='S
 parser.add_argument('--from', type=str, metavar='{address}', nargs=1, help='Email address report should be sent from')
 args=parser.parse_args()
 
+# Create logfile and initialize with header information and cmdline arguments
+# ---------------------------------------------------------------------------
 ts=time.time()
 st=datetime.datetime.fromtimestamp(ts).strftime("%Y%m%d-%H%M%S")
 logfile=logroot+args.query[0].split(".",1)[0]+"-"+st+".log"
 if not os.path.exists(logroot):
     try:
         os.makedirs(logroot)
-    except:
+    except Exception, e:
         print ("FATAL: Can't make log root directory - "+logroot)
+        print (e)
         sys.exit(1)
 if not os.path.exists(logfile):
     try:
         with open(logfile, "w") as l:
-            pass
-    except:
+            logentry(logfile,"*"*40)
+            logentry(logfile,"Script Initiated at "+st)
+            logentry(logfile,"Command Line Parameters:")
+            for arg in vars(args):
+                logentry(logfile,"    "+str(arg)+": "+str(getattr(args,arg)))
+    except Exception, e:
         print ("FATAL: Can't create log file - "+logfile)
+        print (e)
         sys.exit(1)
 
-logentry(logfile,"*"*40)
-logentry(logfile,"Script Initiated at "+st)
-logentry(logfile,"Command Line Parameters:")
-for arg in vars(args):
-    logentry(logfile,"    "+str(arg)+": "+str(getattr(args,arg)))
-
-try:
-    logentry(logfile,'Connecting to the PostgreSQL database..')
-    conn = psycopg2.connect(getpgauth())
-    logentry(logfile,'Connected to database')
-except psycopg2.Error, e:
-    logentry(logfile,'FATAL: Unable to connect to the database. The following error message was generated:')
-    logentry(logfile,e)
-    sys.exit(1)
-
-with open(args.query[0], "r") as queryfile:
-    query=queryfile.read()
-    logentry(logfile,'SQL Query read')
-
-column_names=[]
-data_rows=[]
-with conn.cursor() as cursor:
-    logentry(logfile,'Executing SQL Query')
-    cursor.execute(query)
-    column_names = [desc[0] for desc in cursor.description]
-    for row in cursor:
-        data_rows.append(row)
-
+# Initialize reports directory
+# ----------------------------
 current_directory=os.getcwd()
 reports_directory=os.path.join(current_directory,reports_dir)
 if not os.path.exists(reports_directory):
     try:
         os.makedirs(reports_directory)
-    except:
-        print ("FATAL: Cant make reports directory - "+reports_directory)
+    except Exception, e:
+        logentry(logfile,'FATAL: Can\'t make reports directory - '+reports_directory)
+        logentry(logfile,e)
         sys.exit(1)
 
+# Connect to PostgreSQL database
+# ------------------------------
+try:
+    conn = psycopg2.connect(readconfigfile(sfconfigfile,'pg','pg_uri'))
+    logentry(logfile,'Connected to database')
+except psycopg2.Error, e:
+    logentry(logfile,'FATAL: Unable to connect to the database. The following error message was generated:')
+    logentry(logfile,e)
+    sys.exit(1)
+ 
+# Read SQL query from config file
+# -------------------------------
+try:
+    query=readconfigfile(args.query[0],'sqlquery','query')
+    logentry(logfile,'SQL Query read')
+except Exception, e:
+    logentry(logfile,'FATAL: Unable to read SQL query in '+args.query[0])
+    logentry(logfile,e)
+    sys.exit(1)
+
+# Read SQL variables from config file
+# -----------------------------------
+try:
+    config=ConfigParser.ConfigParser()
+    config.read(args.query[0])
+    qv=(config.options('queryvars'))
+    logentry(logfile,'SQL variables read')
+except Exception, e:
+    logentry(logfile,'FATAL: Unable to read SQL variables in '+args.query[0])
+    logentry(logfile,e)
+    sys.exit(1)
+    
+# Replace variable placeholders in SQL query with those found in config file
+# --------------------------------------------------------------------------
+for qvar in qv:
+    varvalue=config.get('queryvars',qvar)
+    query=query.replace("{{"+qvar+"}}",varvalue)
+
+# Execute SQL query and capture results
+# -------------------------------------
+column_names=[]
+data_rows=[]
+with conn.cursor() as cursor:
+    logentry(logfile,'Executing SQL Query')
+    try:
+        cursor.execute(query)
+        column_names = [desc[0] for desc in cursor.description]
+        for row in cursor:
+            data_rows.append(row)
+    except Exception, e:
+        logentry(logfile,'FATAL: Error during SQL query execution')
+        logentry(logfile,e)
+        sys.exit(1)
+
+# Generate either csv or html report 
+# ----------------------------------
 if args.csv:
     report_file=reports_directory+args.query[0].split(".",1)[0]+"-"+st+"-report.csv"
-    with open(report_file, "w") as rf:
-        rf.write(args.delimiter.join(column_names)+"\n")
-        for row in data_rows:
-            rf.write(args.delimiter.join(str(element) for element in row)+"\n")
+    try:
+        with open(report_file, "w") as rf:
+            rf.write(args.delimiter.join(column_names)+"\n")
+            for row in data_rows:
+                rf.write(args.delimiter.join(str(element) for element in row)+"\n")
+        logentry(logfile,'Report generated: '+report_file)
+    except Exception,e:
+        logentry(logfile,'FATAL: Error during report generation')
+        logentry(logfile,e)
+        sys.exit(1)
 else:
     report_file=reports_directory+args.query[0].split(".",1)[0]+"-"+st+"-report.html"
     
